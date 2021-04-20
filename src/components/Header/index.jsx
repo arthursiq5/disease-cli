9d0eb13fbca8a425146cb1ebad90bc8a4c8134de
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-20 16:03:02 -0300

    Formatada página

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Disease.sh client</a>
+        </div>
+    </nav>;
+}
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,14 @@ import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <div className="container">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
